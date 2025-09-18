8cf07172967e69bafca7ce8cac1a1afae08d8f9f
Ricardo Martínez <rich.mtx1205@gmail.com>
2025-09-18 08:29:05 -0600

    Commit 8
    Cambios en el Config1.js

--- public/config1.js
+++ public/config1.js
@@ -1,3 +1,9 @@
 
-const URL_SERVER = "https://maestria-b40dd.web.app";
+const URL_SERVER = "maestriaadmin-production.up.railway.app";
 export {URL_SERVER};
+
+// maestriaadmin-production.up.railway.app
+
+// https://maestria-b40dd.web.app
+
+// https://maestriaadmin-production.up.railway.app
