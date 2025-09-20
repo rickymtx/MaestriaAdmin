1edae0f36dbc336513a260c0a4eb1e28279febe7
Ricardo Martínez <rich.mtx1205@gmail.com>
2025-09-19 18:41:01 -0600

    Commit10

--- public/config1.js
+++ public/config1.js
@@ -1,9 +1,3 @@
 
 const URL_SERVER = "https://maestriaadmin-production.up.railway.app";
 export {URL_SERVER};
-
-// maestriaadmin-production.up.railway.app
-
-// https://maestria-b40dd.web.app
-
-// https://maestriaadmin-production.up.railway.app
